4c898ede244625c1c494ed07f9d8fbb7b973ee78
zaid822 <57092835+zaid822@users.noreply.github.com>
2019-11-01 20:21:13 +0500

    Add files via upload

---
+++ Untitled15.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ("hello world")
+
+
+# In[5]:
+
+
+name="zaid"
+father_name="sohail"
+university="bahria university"
+message="""
+PIAIC ISLAMABAD BATCH3
+name:{}
+father_name:{}
+university:{}
+""".format(name,father_name,university)
+print(message)
+
+
+# In[6]:
+
+
+weight=250
+print(weight+500)
+print(weight-25)
+print(weight*100)
+print(weight/50)
+
+
+# In[ ]:
+
+
+
+
